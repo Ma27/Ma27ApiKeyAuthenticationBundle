926004e218d830e66966cfd896b8ca2725b3334c
Maximilian Bosch <maximilian.bosch.27@gmail.com>
2016-01-19 19:36:23 +0100

    GH-30 minor fixes

--- Tests/Resources/Entity/IncompleteTestUser.php
+++ Tests/Resources/Entity/IncompleteTestUser.php
@@ -2,11 +2,10 @@
 
 namespace Ma27\ApiKeyAuthenticationBundle\Tests\Resources\Entity;
 
-use Doctrine\ORM\Mapping as ORM;
 use Ma27\ApiKeyAuthenticationBundle\Annotation as Auth;
 
 /**
- * @ORM\Entity()
+ * Simple test object.
  */
 class IncompleteTestUser
 {
